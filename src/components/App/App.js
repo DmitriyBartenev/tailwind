219c5642c6fbc03e109bab9cd6e66ff641270645
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-11-08 14:03:11 +0400

    tailwindcss commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import NavBar from "../NavBar";
+import Main from '../Main';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Main/>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+
+
+
+const NavBar = () => {
+    return(
+        <nav className="relative container mx-auto p-6 ">
+            <div className="flex items-center justify-between">
+                <div className="pt-2 ">
+                    <img src = './assets/logo.svg' alt='Manage logo'/>
+                </div>
+                <div className="hidden md:flex space-x-6 font-medium">
+                    <a href="#" className="hover:text-zinc-700">Pricing</a>
+                    <a href="#" className="hover:text-zinc-700">Product</a>
+                    <a href="#" className="hover:text-zinc-700">About Us</a>
+                    <a href="#" className="hover:text-zinc-700">Careers</a>
+                    <a href="#" className="hover:text-zinc-700">Community</a>
+                </div>
+                <a className="text-white bg-orange-500 py-3 px-6 rounded-full font-bold hover:bg-orange-600">
+                    <button>
+                        Get Started
+                    </button>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
